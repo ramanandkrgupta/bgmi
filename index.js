b62875bbd6d4c1b585e843d322444f31ad000b32
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-11 11:24:53 +0530

    ss

--- index.js
+++ index.js
@@ -59,6 +59,32 @@ app.get('/api/:playerId', async (req, res) => {
     res.status(500).json({ error: 'Failed to retrieve player name' });
   }
 });
+
+app.get("/api", async (req, res) => {
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  try {
+    let browser = await puppeteer.launch(options);
+
+    let page = await browser.newPage();
+    await page.goto("https://www.google.com");
+    res.send(await page.title());
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+});
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
